b4750c4ba32abe1a56c6a01d26489e7d03d53610
Melonvin <1176891456@qq.com>
2021-11-25 17:27:04 +0800

    feat: 🎸 字符串复制到剪切板

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function convertHtmlToMarkdown() {
   // 获取标题
   let title = document.querySelector("h4[data-cypress='QuestionTitle'] a").innerText.replaceAll(' ', '')
   const url = window.location.href
-  title = `# <a href="${url}">${title}</a>`
+  title = `# [${title}](${url})`
 
   // 获取难度
   let degree = document.querySelector('span[data-degree]').innerText
@@ -14,17 +14,30 @@ function convertHtmlToMarkdown() {
     let item = content[i]
     if (item.localName === 'pre') {
       item = item.outerHTML
+      item = item.replaceAll('<strong>', '')
+        .replaceAll('</strong>', '')
     } else {
       item = item.innerHTML
     }
     let temp = item.replaceAll('<code>', '`')
       .replaceAll('</code>', '`')
       .replaceAll('&nbsp;', ' ')
-      .replaceAll('<pre>', '```')
-      .replaceAll('</pre>', '```');
+      .replaceAll('<pre>', '```\n')
+      .replaceAll('</pre>', '```')
+      .replaceAll('<li>', '- ')
+      .replaceAll('</li>', '')
+      .replaceAll('&lt;', '<')
+      .replaceAll('\t', '');
     res = res + `${temp}\n\n`
   }
-  console.log(res)
+
+  res = title + '\n## 题目\n' + res + '\n## 题目大意\n\n' + '\n## 解题思路\n\n' + '\n## 代码\n' + '```js\n```';
+  navigator.clipboard.writeText(res).then(function () {
+    alert('题目已成功提取到剪贴板')
+  }, function (err) {
+    alert('题目提取失败' + err)
+  })
+
 }
 
 
